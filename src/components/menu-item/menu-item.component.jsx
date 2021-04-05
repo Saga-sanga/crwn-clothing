19b9e49c71fe71600bebe28834d38061bb5c3fcb
Saga-sanga <vulcan248@gmail.com>
2021-04-05 17:40:40 +0530

    added signin page component and singinandsignup page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,7 +2,6 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
-  console.log(history, match);
 
    return  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div 
